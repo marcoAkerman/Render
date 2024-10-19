e03e2b6bcba05c8063e2a80e266dbd31cf1be8a2
Marco Antonio Martins Akerman <128430261+marcoAkerman@users.noreply.github.com>
2024-10-18 22:44:05 -0300

    Add files via upload

---
+++ database.py
@@ -0,0 +1,19 @@
+from sqlalchemy import create_engine 
+from sqlalchemy.ext.declarative import declarative_base 
+from sqlalchemy.orm import sessionmaker
+
+# conexão local - MySQL
+#engine = create_engine("mysql+mysqldb://root:@localhost:3306/maternidade")
+
+# conexão supabase - PostgreSQL
+engine = create_engine("postgresql://postgres.gqcbwcfwgpqwbfgcwmfc:4e_E9#T1MyEq@aws-0-sa-east-1.pooler.supabase.com:6543/postgres")
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)  
+Base = declarative_base()
+
+def get_db():     
+    db = SessionLocal()     
+    try:         
+        yield db     
+    finally:
+        db.close()
